972a7a43f81f966cf168feab75b8f578699970fc
Yong.Luo <leleroyn@qq.com>
2023-12-15 16:13:01 +0800

    优化

--- service/Ocr.py
+++ service/Ocr.py
@@ -9,15 +9,16 @@ class Ocr:
         self.rapid_ocr = RapidOCR()
 
     def detect(self, image):
-        # img_w = 1024 if image.shape[1] > 1024 else image.shape[1]
-        # image = cv2.resize(image, (img_w, int(img_w * image.shape[0] / image.shape[1])),
-        #                   interpolation=cv2.INTER_AREA)
+        img_w = 1024 if image.shape[1] > 1024 else image.shape[1]
+        image = cv2.resize(image, (img_w, int(img_w * image.shape[0] / image.shape[1])),
+                           interpolation=cv2.INTER_AREA)
         angle, image = orientation(image)
         B_channel, G_channel, R_channel = cv2.split(image)
-        res, elapse = self.rapid_ocr(R_channel)
+        res, elapse = self.rapid_ocr(R_channel, box_thresh=0.5, unclip_ratio=1.80)
         if res is not None:
             results = []
             for i in range(len(res)):
+                print(res[i][1])
                 item = {"position": res[i][0], "text": res[i][1], "score": res[i][2]}
                 results.append(item)
         return results
