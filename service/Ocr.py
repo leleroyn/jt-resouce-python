f1a24deeaafd1a5b16b259627ec4588fee5ef9e0
Yong.Luo <leleroyn@qq.com>
2023-12-18 10:25:48 +0800

    优化

--- service/Ocr.py
+++ service/Ocr.py
@@ -1,12 +1,11 @@
 import cv2
-from rapidocr_onnxruntime import RapidOCR
-
+from paddleocr import PaddleOCR
 from service.ImageUtil import orientation
 
 
 class Ocr:
     def __init__(self):
-        self.rapid_ocr = RapidOCR()
+        self.ocr = PaddleOCR(use_angle_cls=True, lang="ch")
 
     def detect(self, image):
         img_w = 1024 if image.shape[1] > 1024 else image.shape[1]
@@ -14,11 +13,13 @@ class Ocr:
                            interpolation=cv2.INTER_AREA)
         angle, image = orientation(image)
         B_channel, G_channel, R_channel = cv2.split(image)
-        res, elapse = self.rapid_ocr(R_channel, box_thresh=0.5, unclip_ratio=1.80)
-        if res is not None:
+        result = self.ocr.ocr(R_channel, cls=True)
+        if result is not None:
             results = []
-            for i in range(len(res)):
-                print(res[i][1])
-                item = {"position": res[i][0], "text": res[i][1], "score": res[i][2]}
-                results.append(item)
+            for idx in range(len(result)):
+                res = result[idx]
+                for line in res:
+                    item = {"position": line[0], "text": line[1][0], "score": line[1][1]}
+                    print(line[1][0])
+                    results.append(item)
         return results
